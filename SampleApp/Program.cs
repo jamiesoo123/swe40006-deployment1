3d26efdf818d1f7d45e7f86fafdb213ea9f332ce
jamieQYsoo <jamie.qy.soo@gmail.com>
2026-08-06 20:04:38 +1000

    Pass task completed

---
+++ SampleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deployment Activity 1: Pass Task Completed!");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
